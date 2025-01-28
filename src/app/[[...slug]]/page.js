13697856ddb407f4b4bbb07d548e2ad0fc79c12f
livcheerful <li.v.cheerful@gmail.com>
2025-01-28 11:49:18 -0800

    static export need to fix tina

--- src/app/[[...slug]]/page.js
+++ src/app/[[...slug]]/page.js
@@ -3,8 +3,17 @@ import Navbar from "../components/Navbar";
 import MainMap from "../components/Map";
 import ContentPane from "../components/ContentPane";
 
+export async function generateStaticParams() {
+  const slugs = ["hello-world", "sinking-ship", "", "discover"];
+  const slugmap = slugs.map((s) => ({
+    slug: [s],
+  }));
+  console.log(slugmap);
+  return slugmap;
+}
+
 export default async function Page({ params }) {
-  const slug = (await params).slug;
+  const { slug } = await params;
   let post;
   if (slug && slug != "discover") {
     const p = await fetchPostBySlug(slug[0], "uh..");
