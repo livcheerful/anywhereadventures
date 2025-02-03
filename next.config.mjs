c3fa726b726fd8bfba609ce4d053a8ef4322b8c9
livcheerful <li.v.cheerful@gmail.com>
2025-02-03 13:20:11 -0800

    fix css by adding assetprefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
-/** @type {import('next').NextConfig} */
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 import createMDX from "@next/mdx";
+export default (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    output: "export",
+    distDir: "out",
+    // Configure `pageExtensions` to include markdown and MDX files
+    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+    reactStrictMode: false,
+    assetPrefix: isDev
+      ? undefined
+      : "https://livcheerful.github.io/anywhereadventures/",
+  };
 
-const nextConfig = {
-  output: "export",
-  distDir: "out",
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  reactStrictMode: false,
+  const withMDX = createMDX({});
+  return withMDX(nextConfig);
 };
-
-const withMDX = createMDX({});
-export default withMDX(nextConfig);
