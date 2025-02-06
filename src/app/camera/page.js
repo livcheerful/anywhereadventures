dd472e0820659156ce50bed705263fcfa066cd0b
livcheerful <li.v.cheerful@gmail.com>
2025-02-05 17:46:29 -0800

    saw cheese ui

--- src/app/camera/page.js
+++ src/app/camera/page.js
@@ -8,14 +8,35 @@ export default function Page() {
   const [width, setWidth] = useState(320);
   const [height, setHeight] = useState(0);
   const [photoSources, setPhotoSources] = useState(undefined);
+  const [showSayCheese, setShowSayCheese] = useState(false);
+  const [countdown, setCountdown] = useState(false);
   useEffect(() => {
     getMedia({
       video: true,
     });
   }, []);
+  function stitchImages(images) {
+    document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+
+    images.forEach((i) => {
+      ctx.drawImage(i, 0, 0, width, height);
+    });
+  }
   function takePicture() {
     const context = canvas.getContext("2d");
     console.log("in take picture");
+    setCountdown("3");
+    setTimeout(() => {
+      setCountdown("2");
+    }, 1000);
+    setTimeout(() => {
+      setCountdown("1...");
+    }, 2000);
+    setTimeout(() => {
+      setCountdown(undefined);
+      setShowSayCheese(true);
+    }, 3000);
     if (width && height) {
       function snapPhoto() {
         canvas.width = width;
@@ -30,16 +51,17 @@ export default function Page() {
       setTimeout(() => {
         console.log("snap!");
         filmStrip.push(snapPhoto());
-      }, 300);
+      }, 3500);
       setTimeout(() => {
         console.log("snap!");
         filmStrip.push(snapPhoto());
-      }, 400);
+      }, 4000);
       setTimeout(() => {
         console.log("snap!");
         filmStrip.push(snapPhoto());
         setPhotoSources(filmStrip);
-      }, 900);
+        setShowSayCheese(false);
+      }, 4500);
     } else {
       clearPhoto();
     }
@@ -51,7 +73,6 @@ export default function Page() {
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     const data = canvas.toDataURL("image/png");
-    photo.setAttribute("src", data);
   }
 
   async function getMedia(constraints) {
@@ -87,26 +108,26 @@ export default function Page() {
   }
 
   return (
-    <div className="flex flex-col gap-3 m-2">
-      {photoSources ? (
-        <button
-          className="p-4 bg-violet-200 rounded-lg drop-shadow-sm"
-          onClick={() => {
-            setPhotoSources(undefined);
-          }}
-        >
-          Retake
-        </button>
-      ) : (
-        <button
-          className="p-4 bg-violet-200 rounded-lg drop-shadow-sm"
-          id="start-button"
-          onClick={() => takePicture()}
-        >
-          Take photo
-        </button>
-      )}
-      <div className="filmStrip w-[340px] relative">
+    <div className="flex flex-col relative">
+      <div className="flex flex-row justify-center z-20 absolute bottom-0 w-full">
+        {photoSources ? (
+          <button
+            className="p-4 bg-violet-200 rounded-lg drop-shadow-sm"
+            onClick={() => {
+              setPhotoSources(undefined);
+            }}
+          >
+            Retake
+          </button>
+        ) : (
+          <button
+            className="p-4 w-12 h-12 outline-2 bg-white rounded-full drop-shadow-sm"
+            id="start-button"
+            onClick={() => takePicture()}
+          ></button>
+        )}
+      </div>
+      <div className="filmStrip relative">
         <div className={`${photoSources ? "hidden" : "visible"} camera`}>
           <video
             webkit-playsinline
@@ -116,13 +137,14 @@ export default function Page() {
             muted
             loop
             id="video"
+            className="w-full"
           >
             Video stream not available.
           </video>
         </div>
         <canvas className="hidden" id="canvas" />
         {photoSources && (
-          <div className="output flex flex-col relative ">
+          <div className="output flex flex-col relative w-fit">
             {photoSources.map((p, i) => {
               return (
                 <img
@@ -138,16 +160,23 @@ export default function Page() {
             <img
               className="absolute top-0 left-0"
               style={{ height: height * 3 }}
-              src="/film-left.png"
+              src="film-left.png"
             />
             <img
               className="absolute right-0 top-0"
               style={{ height: height * 3 }}
-              src="/film-right.png"
+              src="film-right.png"
             />
           </div>
         )}
       </div>
+      {(showSayCheese || countdown) && (
+        <div className="absolute z-10 w-full bg-white/50 h-full flex justify-center items-center">
+          <div className="text-white font-black text-[10rem] ">
+            {countdown ? countdown : "say CHEEESE"}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
