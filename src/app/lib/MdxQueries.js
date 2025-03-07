1a70f3780212c9a31d506d075cb288820b634ae2
livcheerful <li.v.cheerful@gmail.com>
2025-03-07 15:02:17 -0800

    content update

--- src/app/lib/MdxQueries.js
+++ src/app/lib/MdxQueries.js
@@ -3,7 +3,7 @@
 export const seattleLocs = [
   {
     "slug": "denny-regrade",
-    "content": "\n\n# The Denny Regrades\n\nWhen Arthur Denny lived in Seattle, he lived on a hill named after himself -- Denny Hill.  \n\n<Comic  position={\"left\"} speechBubbles={[{ text: \"...\" }, {text: \"You **must** be mistaken. **Seattle doesn't have a Denny Hill**.\"}]} image=\"/TR.png\"/>\n\n<Comic  position={\"left\"} speechBubbles={[{text: \"We have First Hill, Capitol Hill\"}, {text: \"Cherry Hill, Beacon Hill, and even a slight deceptive slope up Ballard.\"}]} image=\"/TR.png\"/>\n\n<Comic  position={\"left\"} speechBubbles={[{ text: \"But no Denny Hill\" }]} image=\"/TR.png\"/>\n\n\n\n<Comic  position={\"right\"} speechBubbles={[{ text: \"That's true! Because they tore down a bunch of hills\" }]} image=\"/TR.png\"/>\n\n<LOCItem linkOut=\"https://www.loc.gov/item/2015646669/\" image=\"/loc/cutting-hill.jpg\" caption=\"Cutting down a hill with a powerful jet of water\"/>\n\nDenny imagined that Seattle would be the capitol of the state of Washington with the center of town being in what is now Belltown.\n\nHe wanted to make it easier to access by horse, so the big regrade of Seattle started. The technology of the day was to shoot water at the hills and take the wet clay and dump it into Elliot Bay.\n\n<LOCItem image=\"/loc/regrade-results.jpg\" linkOut=\"https://www.loc.gov/item/2024693820/\" caption=\"The Washington Hotel, where Theodore Roosevelt stayed when he visited Seattle\"/>\n\n<Comic position={\"left\"} speechBubbles={[{ text: \"It was a lot of work, but at least we made a beautiful capitol.\" }]} image=\"/TR.png\"/>\n<Comic position={\"right\"} speechBubbles={[]} image=\"/TR.png\"/>\n<Comic position={\"left\"} speechBubbles={[{ text: \"... what?\" }]} image=\"/TR.png\"/>",
+    "content": "\n\n# The Denny Regrades\n\nWhen Arthur Denny lived in Seattle, he lived on a hill named after himself -- Denny Hill.  \n\n<Comic  position={\"left\"} speechBubbles={[{ text: \"...\" }, {text: \"You must be mistaken. Seattle doesn't have a *Denny Hill.*\"}]} image=\"/comics/denny-regrade/citizen-1.png\"/>\n\n<Comic  position={\"left\"} speechBubbles={[{text: \"We have First Hill, Capitol Hill\"}, {text: \"Cherry Hill, Beacon Hill, and even a slight deceptive slope up Ballard.\"}]} image=\"/comics/denny-regrade/citizen-2.png\"/>\n\n<Comic  position={\"left\"} speechBubbles={[{ text: \"But no Denny Hill\" }]} image=\"/comics/denny-regrade/citizen-3.png\"/>\n\n\n\n<Comic  position={\"right\"} speechBubbles={[{ text: \"That's right! Because we tore it down!\" }]} image=\"/comics/denny-regrade/arthur-1.png\"/>\n\n<LOCItem linkOut=\"https://www.loc.gov/item/2015646669/\" image=\"/loc/cutting-hill.jpg\" caption=\"Cutting down a hill with a powerful jet of water\"/>\n\nDenny imagined that Seattle would be the capitol of the state of Washington with the center of town being in what is now Belltown.\n\nCity engineer Arthur H. Dimmock wanted to make it easier to access by horse, so the big regrade of Seattle started. The technology of the day was to shoot water at the hills and take the wet clay and dump it into Elliot Bay.\n\nThe water was pumped from Lake Union - over *20,000 gallons* of water were used to cut down about 27 city blocks.\n\n<LOCItem image=\"/loc/regrade-results.jpg\" linkOut=\"https://www.loc.gov/item/2024693820/\" caption=\"The Washington Hotel, where Theodore Roosevelt stayed when he visited Seattle\"/>\n\n<Comic position={\"right\"} speechBubbles={[{ text: \"It was a lot of work, but at least we made a beautiful capitol.\" }]} image=\"/comics/denny-regrade/arthur-2.png\"/>\n<Comic position={\"left\"} speechBubbles={[]} image=\"/comics/denny-regrade/citizen-4.png\"/>\n<Comic position={\"right\"} speechBubbles={[{ text: \"... what?\" }]} image=\"/comics/denny-regrade/arthur-3.png\"/>",
     "title": "Moving Mountains Downtown",
     "location": [
       "Seattle"
@@ -14,7 +14,8 @@ export const seattleLocs = [
     ],
     "zoom": 10,
     "tags": [],
-    "cardImage": "/loc/regrade-results.jpg"
+    "cardImage": "/loc/regrade-results.jpg",
+    "cameraType": "stereograph"
   },
   {
     "slug": "joseprizal",
@@ -35,7 +36,7 @@ export const seattleLocs = [
   },
   {
     "slug": "seattleCentral",
-    "content": "\n## Woah there was a food fight!\n\nEven teachers were involved\n\nwowwwww",
+    "content": "\n## Seattle Central College used to be Seattle's first high school\n\n(kinda)\n\n<LOCItem linkOut=\"https://www.loc.gov/resource/sn87093407/1923-05-17/ed-1/?sp=9&r=-0.321,0.093,1.881,1.062,0\" image=\"/loc/food-fight.jpg\"/>",
     "title": "Seattle Central College",
     "location": [
       "Seattle"
@@ -44,15 +45,25 @@ export const seattleLocs = [
       47.616761121594315,
       -122.32169094779286
     ],
-    "zoom": 17,
+    "zoom": 13,
     "tags": [
       ""
     ],
-    "cardImage": "/loc/food-fight.jpg"
+    "cardImage": "/loc/food-fight.jpg",
+    "cameraType": "newspaper",
+    "frameImage": "/loc/camera-images/high-school.png",
+    "imagePlacement": [
+      1080,
+      808
+    ],
+    "imageDimensions": [
+      293,
+      405
+    ]
   },
   {
     "slug": "sinking-ship",
-    "content": "\n\n# Sinking Ship\n\n<LOCItem lcLink=\"https://www.loc.gov/item/75696663/\" image=\"/loc/triangle.jpg\" caption=\"Map of Seattle in 1891\"/>\n\nHere, where Yesler, 1st, and James meet, a triangle piece of land hosts a unique parking garage called the “Sinking Ship”.\n\nBefore this parking garage was built here, it was the site of three Seattle hotels: the Occidental I, the Occidental II, and the Hotel Seattle.\n\n<LOCItem lcLink=\"https://www.loc.gov/item/75696663/\" image=\"/loc/triangle.jpg\" caption=\"Map of Seattle in 1891\"/>\n<Comic position=\"right\" speechBubbles={[{ text: 'This was the first \"fancy\" hotel in Seattle.' }]} image=\"/comics/sinking-ship/g1.png\" overlap={150} absoluteHeight={60}/>\n<Comic speechBubbles={[{ text: 'But it’s cursed! how many hotels stood here before?' }]} image=\"/comics/sinking-ship/g2.png\"/>\n<Comic speechBubbles={[{ text: '...' },{text:\"Three...\"}]} position=\"right\"/>\n<Comic speechBubbles={[{ text: 'And how many got taken out by disasters?' }]} position=\"left\"/>\n<Comic speechBubbles={[{ text: '...two...' }]} position=\"right\"/>\n<Comic speechBubbles={[{ text: 'And tell me, how many hotels are standing there now?' }]} position=\"left\"/>\n<Comic speechBubbles={[{ text: 'none :(' }]} position=\"right\"/>\n<LOCItem lcLink=\"https://www.loc.gov/item/75696663/\" image=\"/loc/triangle.jpg\" caption=\"Map of Seattle in 1891\"/>\n\nThe Occidental II was burned down by the Great Seattle Fire, only 4 years after it had been built to replace the Occidental I.\n\nThe Hotel Seattle, which was built to replace it, was also unlucky -- in 1949, when a huge earthquake shook Olympia, many buildings in Pioneer Square also got damaged. This led to the Hotel Seattle fall into disrepair, and it was the first building to get torn down in a larger plan to renovate Pioneer Square.\n\nIf a building in Pioneer Square is still standing that was around for the 1949 earthquake, you can probably find signs of earthquake damage.\n\n<LOCItem lcLink=\"https://www.loc.gov/item/75696663/\" image=\"/loc/triangle.jpg\" caption=\"Map of Seattle in 1891\"/>\n \n{/* {scheme}://{server}{/prefix}/{identifier}/{region}/{size}/{rotation}/{quality}.{format} */}",
+    "content": "\n\n# Sinking Ship\n\n<LOCItem linkOut=\"https://www.loc.gov/item/75696663/\" image=\"/loc/triangle.jpg\" caption=\"Map of Seattle in 1891\"/>\n\nHere, where Yesler, 1st, and James meet, a triangle piece of land hosts a unique parking garage called the “Sinking Ship”.\n\nBefore this parking garage was built here, it was the site of three Seattle hotels: the Occidental I, the Occidental II, and the Hotel Seattle.\n\n<LOCItem linkOut=\"https://www.loc.gov/resource/gdcmassbookdig.seattlememories00redf/?sp=21\" image=\"/loc/occidental.png\" caption=\"Occidental Hotel\"/>\n<Comic position=\"right\" speechBubbles={[{ text: '*This* was the first \"fancy\" hotel in Seattle.' }]} image=\"/comics/sinking-ship/g1.png\" overlap={150} absoluteHeight={60}/>\n<LOCItem  image=\"/loc/occidental-inside.png\"/>\n<Comic overlap={150} speechBubbles={[{ text: 'But it’s cursed! how many hotels stood here before?' }]} image=\"/comics/sinking-ship/g2.png\"/>\n<Comic speechBubbles={[{ text: '...' },{text:\"Three...\"}]} position=\"right\"/>\n<Comic speechBubbles={[{ text: 'And how many got taken out by disasters?' }]} position=\"left\"/>\n<Comic speechBubbles={[{ text: '...two...' }]} position=\"right\"/>\n<Comic speechBubbles={[{ text: 'And tell me, how many hotels are standing there now?' }]} position=\"left\"/>\n<Comic speechBubbles={[{ text: 'none :(' }]} position=\"right\"/>\n<LOCItem image=\"/loc/hotel-earthquake.png\"/>\n\nThe Occidental II was burned down by the Great Seattle Fire, only 4 years after it had been built to replace the Occidental I.\n\nThe Hotel Seattle, which was built to replace it, was also unlucky -- in 1949, when a huge earthquake shook Olympia, many buildings in Pioneer Square also got damaged. This led to the Hotel Seattle fall into disrepair, and it was the first building to get torn down in a larger plan to renovate Pioneer Square.\n\nIf a building in Pioneer Square is still standing that was around for the 1949 earthquake, you can probably find signs of earthquake damage.\n\n<LOCItem lcLink=\"https://www.loc.gov/item/75696663/\" image=\"/loc/susan-journal.png\" caption=\"Map of Seattle in 1891\"/>\n \n <Comic position=\"right\" overlap={150} image=\"/comics/sinking-ship/g3.png\" speechBubbles={[{text:\"when Susan B. Anthony Visited Seattle, She stayed at the Occidental I.\"},{text:\"That is really cool\"}]}/>\n\n The parking garage that sits here today takes advantage of the unique shape of the land, as well as its grading along a hill.\n{/* {scheme}://{server}{/prefix}/{identifier}/{region}/{size}/{rotation}/{quality}.{format} */}",
     "title": "Sinking Ship Parking Garage",
     "location": [
       "Seattle"
@@ -65,23 +76,8 @@ export const seattleLocs = [
     "cardImage": "/loc/triangle.jpg"
   },
   {
-    "slug": "test",
-    "content": "\n<Comic speechBubbles={[{text:'hello'}]}/>",
-    "title": "Moving Mountains Downtown",
-    "location": [
-      "Seattle"
-    ],
-    "latlon": [
-      47.620815,
-      -122.358086
-    ],
-    "zoom": 10,
-    "tags": [],
-    "cardImage": "/loc/regrade-results.jpg"
-  },
-  {
     "slug": "uw",
-    "content": "\n\n# Hoo Hoo House, University of Washington\n\nThis content is serving now!\n\n<LOCItem caption=\"Exterior\" image=\"/loc/uw-hoo.jpg\" linkOut=\"https://www.loc.gov/resource/hhh.wa0056.photos/?sp=1\"/>",
+    "content": "\n# Hoo Hoo House, University of Washington\n\n<LOCItem caption=\"Exterior\" image=\"/loc/uw-hoo.jpg\" linkOut=\"https://www.loc.gov/resource/hhh.wa0056.photos/?sp=1\"/>",
     "title": "University of Washington",
     "location": [
       "Seattle"
@@ -94,4 +90,4 @@ export const seattleLocs = [
     "cardImage": "/loc/uw-hoo.jpg"
   }
 ];
-export const allSlugs = ["denny-regrade","joseprizal","seattleCentral","sinking-ship","test","uw"]
+export const allSlugs = ["denny-regrade","joseprizal","seattleCentral","sinking-ship","uw"]
