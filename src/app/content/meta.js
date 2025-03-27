60a297c684075eef51b1a89ac83d83ec91fc5205
livcheerful <li.v.cheerful@gmail.com>
2025-03-27 14:05:52 -0700

    remove mayor category

--- src/app/content/meta.js
+++ src/app/content/meta.js
@@ -1,9 +1,8 @@
 export const tagsByCity = {
-  seattle: ["mayors", "infrastructure", "bridges", "downtown", "university"],
+  seattle: ["infrastructure", "bridges", "downtown", "university"],
 };
 
 export const categoryInfo = {
-  mayors: { pinColor: "#75f542", description: "We've had so many mayors!" },
   bridges: { pinColor: "#f29100", description: "Seattle is a city of hills." },
   infrastructure: {
     pinColor: "#f29100",
