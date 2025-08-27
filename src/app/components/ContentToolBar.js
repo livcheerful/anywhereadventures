d4ead56b22c33f5bd5fbbcbf3424e1325df96da8
livcheerful <li.v.cheerful@gmail.com>
2025-08-27 13:08:58 -0700

    change menu to gear icon

--- src/app/components/ContentToolBar.js
+++ src/app/components/ContentToolBar.js
@@ -19,70 +19,6 @@ export default function ContentToolBar({
   const [reduceAnims, setReduceAnims] = useState(false);
   const { notifications } = useNotifications(); // <-- use context
 
-  useEffect(() => {
-    menuAnimRef.current = gsap.timeline({ paused: true });
-
-    menuAnimRef.current.to(
-      "#line1",
-      {
-        y: 15,
-        rotate: 45,
-        transformOrigin: "50% 50%",
-        duration: 0.3,
-      },
-      0
-    );
-
-    menuAnimRef.current.to(
-      "#line3",
-      {
-        y: -15,
-        x: 0,
-        rotate: -45,
-        transformOrigin: "50% 50%",
-        duration: 0.3,
-      },
-      0
-    );
-
-    menuAnimRef.current.to(
-      "#line2",
-      {
-        scaleX: 0,
-        opacity: 0,
-        transformOrigin: "50% 50%",
-        duration: 0.3,
-      },
-      0
-    );
-
-    setReduceAnims(getSettings().reduceAnims);
-  }, []);
-
-  useEffect(() => {
-    if (reduceAnims) {
-      const l1 = document.querySelector("#line1");
-      const l2 = document.querySelector("#line2");
-      const l3 = document.querySelector("#line3");
-      if (showingMenu) {
-        l1.style.transform = "translateY(15px) rotate(45deg)";
-        l2.style.visibility = "hidden";
-        l3.style.transform = "translateY(-15px) rotate(-45deg)";
-      } else {
-        l1.style.transform = "translateY(0px) rotate(0deg)";
-        l2.style.visibility = "visible";
-        l3.style.transform = "translateY(0px) rotate(0deg)";
-      }
-    } else {
-      showMenuAnim(showingMenu);
-    }
-  }, [showingMenu, reduceAnims]);
-
-  function showMenuAnim(shouldShow) {
-    const tl = menuAnimRef.current;
-    shouldShow ? tl.play() : tl.reverse();
-  }
-
   return (
     <div
       className="md:w-limiter rounded-t-lg relative text-xs font-bold flex flex-row gap-2 border-t-gray-800 border-t-2 dark:text-black"
@@ -94,40 +30,14 @@ export default function ContentToolBar({
         <div className="w-3/4 gap-2 flex flex-row">
           <button
             onClick={() => setShowingMenu(!showingMenu)}
-            className="bg-lime-200 active:bg-lime-300 text-black flex flex-col items-stretch justify-center overflow-clip border-b-2 border-l-2 border-r-2 border-gray-800 rounded-b-xl text-center text-xs drop-shadow-2xl fill-black"
+            className="p-2 bg-lime-200 active:bg-lime-300 text-black flex flex-col items-stretch justify-center overflow-clip border-b-2 border-l-2 border-r-2 border-gray-800 rounded-b-xl text-center text-xs drop-shadow-2xl fill-black"
           >
             <svg
-              viewBox="0 0 100 100"
-              id="menu-icon"
-              className="fill-gray-800 w-6 h-6 grow"
+              className="w-5 h-5 fill-gray-600"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
             >
-              <rect
-                className="line"
-                id="line1"
-                x="20"
-                y="30"
-                width="60"
-                height="8"
-                rx="5"
-              />
-              <rect
-                className="line"
-                id="line2"
-                x="20"
-                y="45"
-                width="60"
-                height="8"
-                rx="5"
-              />
-              <rect
-                className="line"
-                id="line3"
-                x="20"
-                y="60"
-                width="60"
-                height="8"
-                rx="5"
-              />
+              <path d="M195.1 9.5C198.1-5.3 211.2-16 226.4-16l59.8 0c15.2 0 28.3 10.7 31.3 25.5L332 79.5c14.1 6 27.3 13.7 39.3 22.8l67.8-22.5c14.4-4.8 30.2 1.2 37.8 14.4l29.9 51.8c7.6 13.2 4.9 29.8-6.5 39.9L447 233.3c.9 7.4 1.3 15 1.3 22.7s-.5 15.3-1.3 22.7l53.4 47.5c11.4 10.1 14 26.8 6.5 39.9l-29.9 51.8c-7.6 13.1-23.4 19.2-37.8 14.4l-67.8-22.5c-12.1 9.1-25.3 16.7-39.3 22.8l-14.4 69.9c-3.1 14.9-16.2 25.5-31.3 25.5l-59.8 0c-15.2 0-28.3-10.7-31.3-25.5l-14.4-69.9c-14.1-6-27.2-13.7-39.3-22.8L73.5 432.3c-14.4 4.8-30.2-1.2-37.8-14.4L5.8 366.1c-7.6-13.2-4.9-29.8 6.5-39.9l53.4-47.5c-.9-7.4-1.3-15-1.3-22.7s.5-15.3 1.3-22.7L12.3 185.8c-11.4-10.1-14-26.8-6.5-39.9L35.7 94.1c7.6-13.2 23.4-19.2 37.8-14.4l67.8 22.5c12.1-9.1 25.3-16.7 39.3-22.8L195.1 9.5zM256.3 336a80 80 0 1 0 -.6-160 80 80 0 1 0 .6 160z" />
             </svg>
           </button>
 
